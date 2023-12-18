144e397598cdd1696aa1c4dcaca1f6acce50fd7e
dustin-ward <wardddustin@gmail.com>
2023-12-18 13:51:56 -0500

    area polygon

---
+++ areapoly.go
@@ -0,0 +1,12 @@
+package complibgo
+
+func AreaPolygon(polygon []Point) double {
+	sum := 0.0
+	n := len(polygon)
+
+	for i := 0; i < n-1; i++ {
+		sum += polygon[i].x*polygon[i+1].y - polygon[i].y*polygon[i+1].x
+	}
+	sum += polygon[n-1].x*polygon[0].y - polygon[n-1].y*polygon[0].x
+	return sum / 2.0
+}
